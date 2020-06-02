3a0e0a4b8eee3a69961394633e918aafdde27421
Vivek Kumar Bansal <vivek.bansal@harness.io>
2020-06-02 14:12:27 +0530

    fix: json-generator-plugin - create directory if does not exists

--- src/webpack/json-generator-plugin.ts
+++ src/webpack/json-generator-plugin.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+import mkdirp from 'mkdirp';
 import type { Compiler } from 'webpack';
 
 export interface JSONGeneratorPluginOptions {
@@ -15,10 +16,14 @@ export default class JSONGeneratorPlugin {
     this.options = options;
   }
 
-  apply(compiler: Compiler) {
+  apply(compiler: Compiler): void {
     const { content, filename } = this.options;
 
-    compiler.hooks.done.tapPromise('JSONGeneratorPlugin', _stats => {
+    compiler.hooks.done.tapPromise('JSONGeneratorPlugin', async _stats => {
+      const output = path.resolve(compiler.outputPath, filename);
+
+      await mkdirp(path.dirname(output));
+
       return fs.promises.writeFile(path.resolve(compiler.outputPath, filename), JSON.stringify(content, null, 2));
     });
   }
