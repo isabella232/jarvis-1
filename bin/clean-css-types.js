884e708caaa21dc98d0bf430f6c7fbf69422fa09
Vivek Kumar Bansal <vivek.bansal@harness.io>
2020-06-10 15:24:46 +0530

    feat: add clean-css-types script

---
+++ bin/clean-css-types.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const cleanCssTypes = require('../lib/misc/clean-css-types').default;
+
+require('yargs')
+  .scriptName('clean-css-types')
+  .command(
+    '$0 <path> [args]',
+    'Find and remove declarations for style (css, scss, etc.) files',
+    yargs => {
+      yargs
+        .positional('path', { description: 'Path to search the definitions' })
+        .option('e', {
+          alias: 'ext',
+          description: 'The extensions to look for. Example: .css, .scss',
+          array: true,
+          default: ['.css']
+        })
+        .check((argv, opt) => {
+          if (!argv.ext.every(ext => ext.startsWith('.'))) {
+            throw new Error('An --ext option must start with a "."');
+          }
+          return true;
+        });
+    },
+    cleanCssTypes
+  ).argv;
---
+++ src/misc/clean-css-types.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+import globby from 'globby';
+
+export interface Options {
+  ext: string[];
+  path: string;
+}
+
+export default async function (options: Options): Promise<void> {
+  // const workingDir = path.resolve(process.cwd(), options.path);
+  const globs = options.ext.map(ext => path.normalize(`${options.path}/**/*${ext}.d.ts`));
+
+  const files = await globby(globs);
+  let deletedCount = 0;
+
+  console.log(`Found ${files.length} declaration files`);
+
+  files.forEach(file => {
+    // for every '.css' there will be a coresponding '.css.d.ts' file and vice versa
+    const cssFile = file.replace(/\.d\.ts$/, '');
+
+    if (!fs.existsSync(cssFile)) {
+      console.log(`Deleting "${file}" because corresponding "${cssFile}" does not exist`);
+      fs.unlinkSync(file);
+      deletedCount++;
+    }
+  });
+
+  console.log(`Deleted total of ${deletedCount} declaration files`);
+}
