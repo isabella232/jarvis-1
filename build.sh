0c1844a3b03a6e7f94bb012d92d1ed71db463bab
Seán Dunne <sean.dunne@harness.io>
2022-01-18 06:59:23 +0000

    feat: [ONP-258]: add source licensing (#10)

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright 2020 Harness Inc. All rights reserved.
+# Use of this source code is governed by the PolyForm Shield 1.0.0 license
+# that can be found in the licenses directory at the root of this repository, also available at
+# https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+
 TYPES_TO_JSON_SCHEMA=(
   'src/jest/common/helpers.ts:CoverageData:src/jest/common/coverage-schema.json'
   'src/jest/jest-grouped-coverage/groupData.ts:Config:src/jest/jest-grouped-coverage/config-schema.json'
--- src/jest/jest-grouped-coverage/table.js
+++ src/jest/jest-grouped-coverage/table.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2020 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
 (() => {
   const COVERAGE_DATA = '__COVERAGE_DATA__'; // this will be replaced by script
   const ID_MAP = '__COVERAGE_TABLE_MAP__'; // this will be replaced by script
